7cad429a2c1e3a6a45d4fadb961d15896c3828a6
Rushil Umaretiya <rushilwiz@gmail.com>
2022-04-16 21:12:58 -0400

    feat(backend): added jwt auth support

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('profile/create', views.ConsumerCreate.as_view()),
     path('profile/', views.ConsumerDetail.as_view()),
     path('foundation/', views.FoundationViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('foundation/<int:pk>/', views.FoundationViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- server/config/settings.py
+++ server/config/settings.py
@@ -106,6 +106,16 @@ else:
         }
     }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 
 # Password validation
